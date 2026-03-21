4bf4f1eeacca45e30ac42503b030e9ca4a59899e
DrakionHub <rm0609726@gmail.com>
2026-03-20 22:05:18 -0300

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,16 +1,17 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, jsonify
 import os
 
 app = Flask(__name__)
 
-# Rota para servir os arquivos da pasta transcripts
+@app.route('/health')
+def health():
+    return jsonify({"status": "online"})
+
 @app.route("/transcript/<filename>")
 def get_transcript(filename):
-    # Obtém o caminho absoluto da pasta transcripts
     directory = os.path.join(os.getcwd(), "transcripts")
-    return send_from_directory(directory, filename)
+    return send_from_directory(directory, filename, mimetype='text/html')
 
 if __name__ == "__main__":
-    # O Railway usa a variável de ambiente PORT, se não houver, usa 3000
     port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
